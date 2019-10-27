225899ec49b9ec8bb370a388dd4ed6cc9ca182fc
Stephen2697 <c16377163@mydit.ie>
2019-10-27 18:42:03 +0000

    Jupyter Import & Commenced Data Processing
    *Analytical Base Table formatting commenced, treating Categorical and Continuous data appropriately.

--- Assignment/DataAnalyser.py
+++ Assignment/DataAnalyser.py
@@ -7,50 +7,94 @@
 
 #Import Modules...
 import sys, os
+import pandas as pd
+import jupyter as jp
 
-#Define Constants
+#-------Define Constants
 FILEINPUT_TXT = "./Data/feature_names.txt"
 FILEINPUT_CSV = "./Data/dataset.csv"
 FILEOUTPUT_CONT = "./Output/C16377163CONT.csv"
 FILEOUTPUT_CAT = "./Output/C16377163CAT.csv"
 HEADING_LIST = []
+CAT_LIST = []
+CONT_LIST = []
+INPUT_DATAFRAME = None
+OUTPUT_DATAFRAME = None
 
-#read in
-def analyseInput():
-	global HEADING_LIST
-	index = 0
-	feature_names = open(FILEINPUT_TXT , "r")
-	
-	for x in feature_names:
-		feature_names = open(FILEINPUT_TXT , "r")
-		HEADING_LIST.append(x.rstrip('\n'))
+#print(INPUT_DATAFRAME.iloc[[23]])
+#for index, col in INPUT_DATAFRAME.iterrows():
+#    print(index)
 
-#deal with CSV headings?
-def initialiseOutput():
-	#Happens once
-	csvFile1 = open(FILEOUTPUT_CONT, "w")
-	csvFile2 = open(FILEOUTPUT_CAT, "w")
-	
-	for item in HEADING_LIST:
-		csvFile1.write("%s," % item)
-		csvFile2.write("%s," % item)
-	
-	for index in range(len(HEADING_LIST)):
-		print(HEADING_LIST(index))
+#-------Define Functions
+def buildInputDataFrame():
+    global HEADING_LIST
+    global INPUT_DATAFRAME
+    feature_Names = open(FILEINPUT_TXT , "r")
+    for feature in feature_Names:
+        feature_Names = open(FILEINPUT_TXT , "r")
+        HEADING_LIST.append(feature.rstrip('\n'))
+        
+    INPUT_DATAFRAME = pd.read_csv(FILEINPUT_CSV,names=HEADING_LIST, keep_default_na=False, na_values=[' ?','',' ','?'])
+    
+def separateDataTypes():
+    global INPUT_DATAFRAME
+    global CAT_LIST
+    global CONT_LIST
+    featureCols = INPUT_DATAFRAME.columns
+    CONT_LIST = INPUT_DATAFRAME._get_numeric_data().columns
+    CAT_LIST = list(set(HEADING_LIST) - set(CONT_LIST))
 
-	csvFile1.close()
-	csvFile2.close()
-	
-#logic to treat data line
-def siftLogic():
-	print("hi")
+def processContinuous(col):
+#	print("CONT ["+ col + "]")
+#	DataFrame.append()
 
-#act on logic and append to appropriate csv
-def splitOutput():
+	#Count
+	count = INPUT_DATAFRAME.shape[0]
 	
-	f = open("myfile.txt", "a")
-	f.write("Woops! I have deleted the content!")
-	f.close()
-
-analyseInput()
-initialiseOutput()
+	#MissingPer
+	nanSum = INPUT_DATAFRAME[[col]].isna().sum()
+	missingPer = 0
+	if int(nanSum):
+		missingPer = round(float(int(nanSum)/count)*100,2)
+		
+		
+	#Cardinality
+	#Min
+	#1stQuart
+	#Mean
+	#Median
+	#3rdQuart
+	#Max
+	#StandDev
+	print((col + ": Count [" + str(count) + "], Missing Per: [" + str(missingPer) + "%]\n").upper())
+	
+def processCategorical(col):
+#	print("CAT ["+ col + "]")
+	#Count
+	count = INPUT_DATAFRAME.shape[0]
+	
+	#MissingPer
+	nanSum = INPUT_DATAFRAME[[col]].isna().sum()
+	missingPer = 0
+	if int(nanSum):
+		missingPer = round(float(int(nanSum)/count)*100,2)
+		
+	print((col + ": Count [" + str(count) + "], Missing Per: [" + str(missingPer) + "%]\n").upper())
+	
+def buildOutputDataFrame():
+    for col in INPUT_DATAFRAME.columns:
+#		Process Continuous Data
+        if col in CONT_LIST:
+            processContinuous(col)
+		
+#		Process Categorical Data
+        elif col in CAT_LIST:
+            
+            processCategorical(col)
+        
+        
+#-------Function Calls & General Logic
+buildInputDataFrame()
+separateDataTypes()
+buildOutputDataFrame()
+exit()
